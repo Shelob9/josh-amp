ec0a8f99e964c5ae48744dc36b70fbbc98f11902
Josh Pollock <jpollock412@gmail.com>
2016-02-27 22:48:13 -0500

    add social sharing

--- josh-amp.php
+++ josh-amp.php
@@ -14,12 +14,12 @@
 add_action( 'amp_post_template_css', function( $amp_template ) {
 	?>
 	nav.amp-wp-title-bar {background: #000;}
-	ul#amp-menu-primary {
+	ul.jp-amp-list {
 		list-style: none;
 		display: inline;
 	}
 
-	ul#amp-menu-primary li {
+	ul.jp-amp-list li {
 		display: inline;
 		margin: 0 8px;
 	}
@@ -51,7 +51,7 @@ add_action( 'pre_amp_render_post', function () {
 		if ( ! empty( $menu ) ) {
 			$menu_items = wp_get_nav_menu_items( $menu->term_id );
 
-			$menu_list = '<ul id="amp-menu-' . $menu_name . '">Menu: ';
+			$menu_list = sprintf( '<br /><ul id="%s" class="jp-amp-list">Menu: ', esc_attr( 'amp-jp-menu-' . $menu_name ) );
 
 			foreach ( $menu_items as $key => $menu_item ) {
 				$menu_list .= sprintf( '<li><a href="%s">%s</a></li>', amp_get_permalink( $menu_item->ID ), esc_html( $menu_item->title ) );
@@ -61,6 +61,29 @@ add_action( 'pre_amp_render_post', function () {
 
 			$content .= $menu_list;
 		}
+
+		return $content;
+	}, 1000 );
+
+	/**
+	 * Add social sharing
+	 */
+	add_filter( 'the_content', function( $content ){
+		$post = get_post();
+		if( is_object( $post ) ){
+			$twitter = add_query_arg( array(
+				'url' => urlencode( get_permalink( $post->ID ) ),
+				'status' => urlencode( $post->post_title )
+			),'https://twitter.com/share' );
+
+			$facebook = add_query_arg( array(
+					'u' => urlencode( get_permalink( $post->ID ) )
+				), 'https://www.facebook.com/sharer/sharer.php'
+			);
+		}
+
+		$share = sprintf( '<hr /><ul id="amp-jp-share" class="jp-amp-list">Share: <li id="twitter-share"><a href="%s" title="Share on Twitter">Twitter</a></li><li id="facebook-share"><a href="%s" title="Share on Facebook">Facebook</a></ul>', esc_url_raw( $twitter ), esc_url_raw( $facebook ) );
+		$content  .= $share;
 
 		return $content;
 	}, 1000 );
